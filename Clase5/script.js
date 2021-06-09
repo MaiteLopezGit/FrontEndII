dc3f7b845e06f711e66bcd2a3c7136dbad11dc6e
Maite Lopez <lopezmaitev@gmail.com>
2021-06-09 19:56:54 -0300

    [ADD] Correccion del nombre de la funcion a un nombre mas descriptivo, Quite los saltos de linea de mas, agregue un transition en el body, aunque no es buena practica, para no ver un cambio tan brusco

--- Clase5/script.js
+++ Clase5/script.js
@@ -1,12 +1,5 @@
-
-
-function darkLightFunction(){
-
-   
-    const body = document.querySelector('body');
-    
-   
-        body.classList.toggle('dark');    
-    
+//Ponerle nombre más descriptivo a las funciones, usar verbos
+function changeDarkLigthMode() {
+  const body = document.querySelector("body");
+  body.classList.toggle("dark");
 }
-
